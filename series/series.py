50addc79a9d2df9bab87efc96a733019643b65ae
eureka84 <angelosciarra@ymail.com>
2019-03-25 08:11:06 +0100

    [DONE] series

--- series/series.py
+++ series/series.py
@@ -1,8 +1,7 @@
 def slices(series, length):
     if len(series) < length:
-        raise ValueError("cannot extract series of length {} from {}".format(length, series))
+        raise ValueError("cannot extract slices of length {} series {}".format(length, series))
     if length < 1:
-        raise ValueError("series length cannot be less than 1")
+        raise ValueError("slices length cannot be less than 1")
     candidates = [series[i:i+length] for i in range(0, len(series))]
     return filter(lambda c: len(c) == length, candidates)
-
